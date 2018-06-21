fb4ccbf8f0d2cc2a4b8e7afbf8a36a1d5f007ee5
8-shen <664905885@qq.com>
2018-06-21 19:19:07 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: __dirname + "/core/index.js", //已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/demo/lib", //打包后的文件存放的地方
+    path: __dirname + "/dist", //打包后的文件存放的地方
     filename: "jQuery-SPA.min.js" //打包后输出文件的文件名
   },
   module: {
